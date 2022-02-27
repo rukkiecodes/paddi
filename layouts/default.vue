b3424a9393e92b6beaadc0b9a0cc1b287e51adef
Terry Amagboro <rukkiecodes@gmail.com>
2022-02-27 04:35:53 +0100

    done with footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -78,8 +78,81 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer absolute app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer absolute dark app color="#362365" class="footer">
+      <img class="img1" src="../assets/img/Frame 49.png" alt="" />
+      <v-row justify="space-between" align="start" no-gutters>
+        <v-col cols="12">
+          <v-container class="py-16">
+            <v-row justify="space-between" align="start">
+              <v-col cols="12" sm="6" md="3" class="d-flex flex-column">
+                <p class="text-h6 font-weight-light">Commpany</p>
+                <span class="text-body-2 mt-3">Testimonials</span>
+                <span class="text-body-2 mt-3">P2P Transaction</span>
+                <span class="text-body-2 mt-3">FAQs</span>
+                <span class="text-body-2 mt-3">Plog</span>
+                <span class="text-body-2 mt-3">Careers</span>
+              </v-col>
+              <v-col cols="12" sm="6" md="3" class="d-flex flex-column">
+                <p class="text-h6 font-weight-light">Support</p>
+                <span class="text-body-2 mt-3">Security</span>
+                <span class="text-body-2 mt-3">Fee Calculator</span>
+                <span class="text-body-2 mt-3">Fraud Prevention</span>
+                <span class="text-body-2 mt-3">Report Bug</span>
+              </v-col>
+              <v-col cols="12" sm="6" md="3" class="d-flex flex-column">
+                <p class="text-h6 font-weight-light">Reach Us</p>
+                <span class="text-body-2 mt-3"
+                  ><v-icon class="mr-2">mdi-email-outline</v-icon>
+                  contact@Trustpaddi.com</span
+                >
+                <span class="text-body-2 mt-3"
+                  ><v-icon class="mr-2">mdi-phone-outline</v-icon>
+                  +2349033275449</span
+                >
+                <span class="text-body-2 mt-3"
+                  ><v-icon class="mr-2">mdi-map-marker-outline</v-icon>
+                  Lagos</span
+                >
+              </v-col>
+              <v-col cols="12" sm="6" md="3" class="d-flex flex-column">
+                <p class="text-h6 font-weight-light">Stay up to data</p>
+                <v-text-field
+                  solo
+                  flat
+                  rounded
+                  full-width
+                  hide-details
+                  background-color="#2E1E56"
+                  placeholder="Enter address"
+                  append-icon="mdi-send-outline mdi-rotate-315"
+                ></v-text-field>
+
+                <div class="d-flex justify-start align-start mt-6">
+                  <v-btn fab small depressed color="#402E6D">
+                    <img src="@/assets/img/u_facebook-f.png" alt="" />
+                  </v-btn>
+                  <v-btn fab small depressed color="#402E6D" class="mx-5">
+                    <img src="@/assets/img/u_twitter.png" alt="" />
+                  </v-btn>
+                  <v-btn fab small depressed color="#402E6D">
+                    <img src="@/assets/img/u_instagram-alt.png" alt="" />
+                  </v-btn>
+                </div>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+        <v-col cols="12">
+          <v-divider />
+          <v-container class="d-flex justify-center">
+            <span class="text-body-2"
+              >&copy; {{ new Date().getFullYear() }} TrustPaddi. All rights
+              reserved</span
+            >
+          </v-container>
+        </v-col>
+      </v-row>
+      <img class="img2" src="../assets/img/Frame 49 2.png" alt="" />
     </v-footer>
   </v-app>
 </template>
@@ -171,6 +244,22 @@ export default {
 .active {
   &::before {
     display: none !important;
+  }
+}
+
+.footer {
+  overflow: hidden;
+  .img1 {
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    z-index: -1;
+  }
+  .img2 {
+    position: absolute;
+    bottom: -230px;
+    right: 0;
+    z-index: -1;
   }
 }
 </style>
